2a726c0be1ed74cc731f31847f9771b209751b12
Lietjefrietje <liannehelderman8@gmail.com>
2025-06-27 08:57:38 +0200

    Add basic basketball scorekeeper app
    Initial commit with HTML, CSS, JS, and font for a simple basketball scorekeeper. Includes UI for home and guest teams, custom font, and buttons to increment scores.

---
+++ index.js
@@ -0,0 +1,28 @@
+let Elscore = document.getElementById("score1")
+
+function plus1() {
+    Elscore.innerText = Number(Elscore.innerText) + 1
+}
+
+function plus2() {
+    Elscore.innerText = Number(Elscore.innerText) + 2
+}
+
+function plus3() {
+    Elscore.innerText = Number(Elscore.innerText) + 3
+}
+
+
+let Elscore2 = document.getElementById("score2")
+
+function add1() {
+    Elscore2.innerText = Number(Elscore2.innerText) + 1
+}
+
+function add2() {
+    Elscore2.innerText = Number(Elscore2.innerText) + 2
+}
+
+function add3() {
+    Elscore2.innerText = Number(Elscore2.innerText) + 3
+}
